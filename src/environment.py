7e2ec39959ac3018c4928e901518103981abfc5f
Kilian Testard <pc47xiy@eduvaud.ch>
2025-02-10 16:00:54 +0100

    refactor: cleaned up the comments

--- src/environment.py
+++ src/environment.py
@@ -9,23 +9,24 @@ class Platform:
         self.moving = moving
         self.speed = speed
         self.move_range = move_range
-        self.start_x = x  # Position initiale pour le mouvement horizontal
-        self.direction = 1  # 1 = droite, -1 = gauche
+        self.start_x = x  # initial position
+        self.direction = 1  # 1 = right, -1 = left
 
     def update(self):
-        """Met à jour la position de la plateforme si elle est mobile (mouvement horizontal)."""
+        # updates the mobile platform's position
         if self.moving:
             self.rect.x += self.speed * self.direction
             if abs(self.rect.x - self.start_x) >= self.move_range:
-                self.direction *= -1  # Inverser la direction
+                self.direction *= -1  # invert the direction
 
     def draw(self, surface, color=(255, 255, 255)):
+        # draws the platform
         pygame.draw.rect(surface, color, self.rect)
 
 
-# Création des plateformes. à priori à mettre dans la boucle, après l'initiation de Pygame
+# Création des plateformes: à priori à mettre dans la boucle, après l'initiation de Pygame
 #platforms = [
-    #Platform(200, 175, 400, 50),  # Plateforme fine
-    #Platform(700, 350, 350, 50),  # Plateforme épaisse
-    #Platform(350, 550, 250, 50, moving=True, speed=3, move_range=250)  # Plateforme mobile
+    #Platform(200, 175, 400, 50),
+    #Platform(700, 350, 350, 50),
+    #Platform(350, 550, 250, 50, moving=True, speed=3, move_range=250)
 #]
