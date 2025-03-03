64115b59711fd2b5f1410b7bc7fa3528bafc8beb
Alex Kamano <pk86onf@eduvaud.ch>
2025-03-03 09:23:35 +0100

    feat: news features about the selectionb of the levels

--- src/game.py
+++ src/game.py
@@ -1,5 +1,7 @@
 #sleep permet de suspendre l'exécution d'un programme pendant une durée spécifique
 from time import sleep
+from unittest import case
+
 import pygame
 import pygame_menu
 from pygame_menu import themes
@@ -32,6 +34,54 @@ selected_difficulty = 'Easy'
 def level_selection():
     menu._open(world)
 
+    match world:
+        case 1:
+            return 1
+        case 2:
+            return 2
+        case 3:
+            return 3
+        case _:
+            return None
+
+def play_level():
+
+    running = True
+
+    while running:
+
+        events = pygame.event.get()
+        death = Collide_damage
+        Player = entities.Player
+        Enemy = entities.Enemy
+
+        for events in pygame.event.get():
+            if events.type == pygame.QUIT:
+                running = False
+
+        key = pygame.key.get_pressed()
+        if key[pygame.K_LEFT]:
+            player1.move(-player1.velocity, 0)
+        if key[pygame.K_RIGHT]:
+            player1.move(player1.velocity, 0)
+        if key[pygame.K_UP]:
+            player1.move(0, -player1.velocity)
+        if key[pygame.K_DOWN]:
+            player1.move(0, player1.velocity)
+
+        pygame.display.flip()
+
+        # Enemy AI: Move towards the player
+        enemy.move_towards_player(player1)
+
+        # pygame.draw.rect(window, (255,0,0), player)
+        player1.draw(screen)
+        enemy.draw(screen)
+        health_bar.draw(screen)
+
+        pygame.display.update()
+
+
 #def set_level(value, level):
 
 def set_diffulty(value, difficulty):
@@ -48,6 +98,28 @@ def start_game():
     pygame.time.set_timer(update_loading, 30)
     # Définir un temps après lequel le chargement est terminé (ex: 3 secondes)
     pygame.time.set_timer(END_LOADING, 3000)  # 3000 ms = 3 secondes
+
+    if set_difficulty == 'Easy' and selected_level == 1:
+        pass
+    elif set_difficulty == 'Easy' and selected_level == 2:
+        pass
+    elif set_difficulty == 'Easy' and selected_level == 3:
+        pass
+    elif set_difficulty == 'Medium' and selected_level == 1:
+        pass
+    elif set_difficulty == 'Medium' and selected_level == 2:
+        pass
+    elif set_difficulty == 'Medium' and selected_level == 3:
+        pass
+    elif set_difficulty == 'Hard' and selected_level == 1:
+        pass
+    elif set_difficulty == 'Hard' and selected_level == 2:
+        pass
+    elif set_difficulty == 'Hard' and selected_level == 3:
+        pass
+
+    play_level()
+
 
 def level_menu():
     menu._open(level)
@@ -70,7 +142,7 @@ def create_menu():
 menu = create_menu()
 
 level = pygame_menu.Menu("Levels", 1400, 800, theme=themes.THEME_BLUE)
-level.add.selector('Difficulty: ', [('Easy',1), ('Medium',2), ('Hard',3)], onchange=set_difficulty)
+level.add.selector('world: ', [('easy',1), ('medium',2), ('hard',3)], onchange=set_difficulty)
 
 loading = pygame_menu.Menu('Loading the Game...', 1400, 800, theme=themes.THEME_DARK)
 loading.add.progress_bar("Progress", progressbar_id= "1", default=0, width=200)
@@ -79,7 +151,7 @@ update_loading = pygame.USEREVENT + 0
 arrow = pygame_menu.widgets.LeftArrowSelection(arrow_size = (10, 15))
 
 world = pygame_menu.Menu("Choose your level...", 1400, 800, theme=themes.THEME_SOLARIZED)
-world.add.selector('Level: ', [('Easy','Easy'), ('Medium', 'Medium'), ('Hard','Hard')], onchange=set_difficulty)
+world.add.selector('Level: ', [('World 1:1','1'), ('world 1:2', 'Medium'), ('World 1:3','Hard')], onchange=start_game())
 world.add.button("Back", pygame_menu.events.BACK)
 world.add.button("Start Game", start_game)
 
@@ -98,29 +170,19 @@ run = True
 while run:
 
     events = pygame.event.get()
-    death = Collide_damage
-    Player = entities.Player
-    Enemy = entities.Enemy
+    #death = Collide_damage
+    #Player = entities.Player
+    #Enemy = entities.Enemy
 
-    key = pygame.key.get_pressed()
-    if key[pygame.K_LEFT]:
-        player1.move(-player1.velocity, 0)
-    if key[pygame.K_RIGHT]:
-        player1.move(player1.velocity, 0)
-    if key[pygame.K_UP]:
-        player1.move(0, -player1.velocity)
-    if key[pygame.K_DOWN]:
-        player1.move(0, player1.velocity)
-
-    if player1.hitbox_player.colliderect(enemy.hitbox_enemy):
-        result = health_bar.decrease_health(2)
-        print("Collision detected! Health decreased.")
-        if result == "retry":
-            Player.reset(player1)
-            Enemy.reset(enemy)
-            health_bar.reset()
-        elif result == "exit" or result == pygame.QUIT:
-            run = False  # quit the game
+    #key = pygame.key.get_pressed()
+    #if key[pygame.K_LEFT]:
+     #   player1.move(-player1.velocity, 0)
+    #if key[pygame.K_RIGHT]:
+     #   player1.move(player1.velocity, 0)
+    #if key[pygame.K_UP]:
+     #   player1.move(0, -player1.velocity)
+    #if key[pygame.K_DOWN]:
+     #   player1.move(0, player1.velocity)
 
     pygame.display.flip()
 
@@ -142,15 +204,25 @@ while run:
             arrow.draw(screen, menu.get_current().get_selected_widget())
 
     # Enemy AI: Move towards the player
-    enemy.move_towards_player(player1)
+    #enemy.move_towards_player(player1)
 
     #pygame.draw.rect(window, (255,0,0), player)
-    player1.draw(screen)
-    enemy.draw(screen)
-    health_bar.draw(screen)
+    #player1.draw(screen)
+    #enemy.draw(screen)
+    #health_bar.draw(screen)
 
     pygame.display.update()
-
+"""
+    if player1.hitbox_player.colliderect(enemy.hitbox_enemy):
+        result = health_bar.decrease_health(2)
+        print("Collision detected! Health decreased.")
+        if result == "retry":
+            Player.reset(player1)
+            Enemy.reset(enemy)
+            health_bar.reset()
+        elif result == "exit" or result == pygame.QUIT:
+            run = False  # quit the game
+"""
 
 """
     if player1.hitbox_player.colliderect(enemy.hitbox_enemy):
@@ -175,3 +247,4 @@ while run:
                 else:
                     collision_occurred = False
 """
+
