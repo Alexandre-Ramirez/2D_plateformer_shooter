a9d80644fca8ecb65f2a03c330838b4bac2e1b62
Alex Kamano <pk86onf@eduvaud.ch>
2025-03-11 08:59:56 +0100

    Error: i did the correction of the display of the code on develop

--- src/game.py
+++ src/game.py
@@ -17,15 +17,15 @@ x,y = screen.get_size()
 
 #setup window's title
 pygame.display.set_caption("Plateformer")
-#setup bg color
-#color = (125,125,125)
 
-#def draw_menu(screen):
-    #take the size of the screen
+
+#take the size of the screen
 screen_width, screen_height = screen.get_size()
 
-# Crée un nouvel événement personnalisé pour signaler la fin du chargement
+# create a new event to telle when the loading is over
 END_LOADING = pygame.USEREVENT +1
+
+#level by default
 selected_level = 'Easy'
 selected_difficulty = 'Easy'
 
@@ -34,6 +34,7 @@ def level_selection():
 
 #def set_level(value, level):
 
+#the player set a difficulty
 def set_diffulty(value, difficulty):
     global selected_level
     selected_level = level
@@ -41,7 +42,7 @@ def set_diffulty(value, difficulty):
 
     #print(value)
     #print(difficulty)
-
+#define with the choosen level the start of the game
 def start_game():
     print(f"Starting game with level : {selected_level}")
     menu._open(loading)
@@ -52,6 +53,7 @@ def start_game():
 def level_menu():
     menu._open(level)
 
+#create a main menu when starting the game
 def create_menu():
     menu = pygame_menu.Menu(
         height= screen_height,
@@ -67,27 +69,36 @@ def create_menu():
 
     return menu
 
+#define the characteristic of each menu
+#define create_menu by menu
 menu = create_menu()
 
+#define the display of the level's menu
 level = pygame_menu.Menu("Levels", 1400, 800, theme=themes.THEME_BLUE)
 level.add.selector('Difficulty: ', [('Easy',1), ('Medium',2), ('Hard',3)], onchange=set_difficulty)
 
+#define the display of loading
 loading = pygame_menu.Menu('Loading the Game...', 1400, 800, theme=themes.THEME_DARK)
 loading.add.progress_bar("Progress", progressbar_id= "1", default=0, width=200)
 update_loading = pygame.USEREVENT + 0
 
+#define the display of the the mouse
 arrow = pygame_menu.widgets.LeftArrowSelection(arrow_size = (10, 15))
 
+#define the display of the level's selector
 world = pygame_menu.Menu("Choose your level...", 1400, 800, theme=themes.THEME_SOLARIZED)
 world.add.selector('Level: ', [('Easy','Easy'), ('Medium', 'Medium'), ('Hard','Hard')], onchange=set_difficulty)
 world.add.button("Back", pygame_menu.events.BACK)
 world.add.button("Start Game", start_game)
 
+#give the details about the player
 player1 = Player(100, 100, 50, 50)
 enemy = Enemy(200, 200, 50, 50)
 
+#define the collision
 collide_damage = Collide_damage(x=0, y=0, max_damage=10)
 
+#define the health_bar
 health_bar = HealthBar(250,200,300,40,100, collide_damage=collide_damage)
 
 #cooldown timer
@@ -95,13 +106,16 @@ last_collision_time = 0
 cooldown = 1
 
 run = True
+#main loop
 while run:
 
+    #define the characteristic
     events = pygame.event.get()
     death = Collide_damage
     Player = entities.Player
     Enemy = entities.Enemy
 
+    #movements of the player
     key = pygame.key.get_pressed()
     if key[pygame.K_LEFT]:
         player1.move(-player1.velocity, 0)
@@ -112,17 +126,23 @@ while run:
     if key[pygame.K_DOWN]:
         player1.move(0, player1.velocity)
 
+    #define the hitbox with the health bar
     if player1.hitbox_player.colliderect(enemy.hitbox_enemy):
+        #the player lose 2hp of life
         result = health_bar.decrease_health(2)
+        #test if the collision is regonize
         print("Collision detected! Health decreased.")
+        #if the player loose all his life a window pop up and he can choose between "retry" and "exit"
+        #if he clicks on retry the game reset
         if result == "retry":
-            Player.reset(player1)
-            Enemy.reset(enemy)
-            health_bar.reset()
+            Player.reset(player1) #the player reset at his origin place
+            Enemy.reset(enemy) #the enemy reset at his origin place
+            health_bar.reset() #the health bar reset at 100
+            #if he clicks on exit
         elif result == "exit" or result == pygame.QUIT:
             run = False  # quit the game
 
-    pygame.display.flip()
+    pygame.display.flip() #update the screen
 
     for event in events:
         if event.type == update_loading:
@@ -144,7 +164,7 @@ while run:
     # Enemy AI: Move towards the player
     enemy.move_towards_player(player1)
 
-    #pygame.draw.rect(window, (255,0,0), player)
+    #draw at the screen
     player1.draw(screen)
     enemy.draw(screen)
     health_bar.draw(screen)
