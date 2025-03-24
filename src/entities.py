011ed5838de6a0d5dbe29f6a53e1d881085f1435
Alex Kamano <pk86onf@eduvaud.ch>
2025-03-24 14:46:32 +0100

    release 1.0

--- src/entities.py
+++ src/entities.py
@@ -1,132 +1,290 @@
 import sys
-
+import os
 import pygame
-import math
-screen = pygame.display.set_mode((1400, 800),pygame.RESIZABLE)
+from environment import Worlds
+
+screen = pygame.display.set_mode((1400, 800))
+screen_width, screen_height = screen.get_size()
+
+my_world = Worlds()
 
 class Player():
-    def __init__(self, x, y, width, height):
+    def __init__(self,x, y, scale, velocity):
         self.x = x
         self.y = y
-        self.width = width
-        self.height = height
+        self.width = 140
+        self.height  = 140
+        self.flip = False
         self.velocity = velocity
-        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
-        self.hitbox_w = self.width + 5
-        self.hitbox_h = self.height + 5
-        self.update_hitbox()
+        self.direction = 1
+        self.screen_width = 1400
+        self.SCROLL_THRESH = self.screen_width / 2
 
-    def update_hitbox(self):
-        self.hitbox_player = pygame.Rect(
-            self.x +(self.width-self.hitbox_w)//2,
-            self.y +(self.height - self.hitbox_h)//2,
-            self.hitbox_w,
-            self.hitbox_h
-        )
+        #add jumping
+        self.jumping = False
+        self.vel_y = 0
+        self.jump_height = 15
+        self.gravity = 9.81
+        self.on_ground = True
 
-    def draw(self, surface):
-
-        pygame.draw.rect(surface, 'red', self.rect)
-        pygame.draw.rect(surface, 'black', self.hitbox_player,2)
-
-    def move(self, dx, dy):
-        #garde l'ancienne position avant le déplacement
-        old_x = self.x
-        old_y = self.y
-
-        #déplacement
-        self.x += dx
-        self.y += dy
-
+        #moves limit
         self.limite_x = (60, 1340)
         self.limite_y = (60, 740)
 
-        #Màj du joueur
-        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        #sprite
+        #animation sprite
+        self.animation_list = []
+        self.frame_index = 0
+        self.action = 0
+        self.update_time = pygame.time.get_ticks()
+        #load all the image for the player
+        animation_types = ['idle', 'run', 'jump', 'crunch']
+        for animation in animation_types:
+            #reset temporary the list of image
+            temp_list = []
+            #count number of files in the folder
+            folder_path = f'image/player/{animation}'
+            #Check if the folder exist before charging the images
+            if os.path.exists(folder_path):
+                #Take the orginise folder
+                files = sorted([f for f in os.listdir(folder_path) if f.endswith('.png')])
+
+                #load the images and scale them
+                for file in files:
+                    img_path = os.path.join(folder_path, file)
+                    img = pygame.image.load(img_path)
+                    img = pygame.transform.scale(img, (self.width, self.height))
+                    temp_list.append(img)
+                self.animation_list.append(temp_list)
+
+        self.image = self.animation_list[self.action][self.frame_index]
+        self.rect = self.image.get_rect()
+        self.rect.center = (x,y)
+
+        # hitbox
+        self.hitbox_w = int(self.width * 0.3)
+        self.hitbox_h = int(self.height * 0.5 + 5)
         self.update_hitbox()
 
-        #vérif des limites
+        self.hp = 100
+
+    def update_hitbox(self):
+        #met à jour la hitbox
+        if self.direction == 1:
+            self.hitbox_player = pygame.Rect (self.rect.x + (self.width - self.hitbox_w) // 2 - 8, self.rect.y + (self.height - self.hitbox_h) // 2 + 35, self.hitbox_w, self.hitbox_h)
+        else:
+            self.hitbox_player = pygame.Rect(self.rect.x + (self.width - self.hitbox_w) // 2 + 8, self.rect.y + (self.height - self.hitbox_h) // 2 + 35, self.hitbox_w, self.hitbox_h)
+
+    #draw the player on the screen
+    def draw(self, surface):
+        surface.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
+        pygame.draw.rect(surface, (255, 0, 0), self.hitbox_player, 2)
+
+    #function to move the player
+    def move(self, moving_l, moving_r,moving_h,moving_d, jumping, on_ground):
+
+        screen_scroll = 0 #scroll the screen
+        dx=0 #x position
+        dy=0 #y position
+
+        #move left
+        if moving_l:
+            dx = -self.velocity
+            self.flip = True
+            self.direction = -1
+        #move right
+        if moving_r:
+            dx = self.velocity
+            self.flip = False
+            self.direction = 1
+        #move up
+        if moving_h:
+            dy = -self.velocity
+            self.flip = False
+            self.direction = -1
+        #move down
+        if moving_d:
+            dy = self.velocity
+            self.flip = False
+            self.direction = 1
+        #jump
+        if jumping and on_ground == False:
+            self.vel_y = -11
+            self.jump = False
+            self.in_air = True
+
+            if not self.on_ground:
+                self.vel_y += self.gravity
+                self.y += self.vel_y
+                self.rect.topleft = (self.x, self.y)
+
+            """
+             # check collision with floor
+            if self.rect.bottom + dy > 300:
+                dy = 300 - self.rect.bottom
+                self.in_air = False
+            """
+            #check the collision
+            print(f"Liste des obstacle: {my_world.obstacles_list}")
+            # check collision with obstacle
+            for tile in my_world.obstacles_list:
+                # Impression pour voir la position du joueur et des obstacles
+                print(f"Joueur - Rect : ({self.rect.x}, {self.rect.y}), Vitesse Y : {self.vel_y}")
+                print(f"Obstacle - Rect : ({tile[1].x}, {tile[1].y}), Taille : ({tile[1].width}, {tile[1].height})")
+                # check for colision in the y direction
+                if tile[1].colliderect(self.rect.x, self.rect.y + self.vel_y, self.width, self.height):
+                    print(f"Collision détectée !")
+                    if self.vel_y >= 0:  # player falling
+                        self.y = tile[1].top - self.height  # put the player on top of the obstacle
+                        self.vel_y = 0  # stop the falling
+                        self.on_ground = True  # the player is on the ground
+                        break
+                    elif self.vel_y < 0:  # the player jump
+                        self.y = tile[1].bottom  # the player goes on top of the obstacle
+                        self.vel_y = 0  # stop the movement
+                        break
+
+            # update rectangle position
+            self.rect.x += dx
+            self.rect.y += dy
+
+        #keep the old positioning before moving
+        old_x = self.x
+        old_y = self.y
+
+        #update the movement
+        self.x += dx
+        self.y += dy
+
+        # update the player
+        self.rect.topleft = (self.x, self.y)
+        self.update_hitbox()
+
+        #maj scroll based on player position
+        if self.rect.right > self.screen_width - self.SCROLL_THRESH or self.rect.left < self.SCROLL_THRESH:
+            self.rect.x -= -dx
+            screen_scroll = -dx
+
+        #check the limits
         if self.rect.left < self.limite_x[0] or self.rect.right > self.limite_x[1]:
             self.x = old_x
-            self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
 
         if self.rect.top < self.limite_y[0] or self.rect.bottom > self.limite_y[1]:
             self.y = old_y
-            self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+
+        return screen_scroll
+
+    def update_animation(self):
+        #update aniation
+        ANIMATION_COOLDOWN = 100
+        #update image depending on current frame
+        self.image = self.animation_list[self.action][self.frame_index]
+        #check if enough time has passed since the last update
+        if pygame.time.get_ticks() - self.update_time > ANIMATION_COOLDOWN:
+            self.update_time = pygame.time.get_ticks()
+            self.frame_index += 1
+        #if the animation has run out the reset back to the start
+        if self.frame_index >= len(self.animation_list[self.action]):
+            self.frame_index = 0
+
+    def update_action(self, new_action):
+        # check if the new action is different to the previous one
+        if new_action != self.action:
+            self.action = new_action
+            # update the animation settings
+            self.frame_index = 0
+            self.update_time = pygame.time.get_ticks()
+
+
     def reset(self):
         self.x = self.x
         self.y = self.y
         self.rect.topleft = (self.x, self.y)
+
+class Enemy:
+    def __init__(self, platform):
+        self.width = 150
+        self.height = 150
+        self.platform = platform
+        self.rect = pygame.Rect(platform.rect.x + platform.rect.width // 2 - self.width // 2, platform.rect.y - self.height, self.width, self.height)
+        self.speed = 1
+        self.direction = 1
+        self.detection_range = 400
+        self.shooting = False
+        self.last_shot_time = 0
+
+        self.image1 = pygame.image.load("image/enemy/base_soldier.png")
+        self.image1 = pygame.transform.scale(self.image1, (self.width, self.height))
+        self.image2 = pygame.image.load("image/enemy/mid-ak47.png")
+        self.image2 = pygame.transform.scale(self.image2, (self.width, self.height))
+        self.image3 = pygame.image.load("image/enemy/high-magnum.png")
+        self.image3 = pygame.transform.scale(self.image3, (self.width, self.height))
+
+        # Assign HP to the 3 enemy types
+        if platform.platform_id in [9, 10]:
+            self.hp = 120
+        elif platform.platform_id == 11:
+            self.hp = 160
+        else:
+            self.hp = 80
+
+        self.hitbox_w = int(self.width * 0.3)
+        self.hitbox_h = int(self.height * 0.5)
         self.update_hitbox()
 
-class Enemy():
-    def __init__(self, x, y, width, height):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
-        self.velocity = velocity - 4
+    def detect_player(self, player):
+        # measure the distance
+        distance_x = abs(self.hitbox_enemy.centerx - player.hitbox_player.centerx)
+        vertical_overlap = (player.hitbox_player.bottom >= self.hitbox_enemy.top and player.hitbox_player.top <= self.hitbox_enemy.bottom)
 
-        self.hitbox_w = width + 5
-        self.hitbox_h = height + 5
-        self.update_hitbox()
+        # turn the enemy around and start shooting
+        if distance_x <= self.detection_range and vertical_overlap:
+            self.shooting = True
+            if player.hitbox_player.centerx < self.hitbox_enemy.centerx:
+                self.direction = -1
+            else:
+                self.direction = 1
+        else:
+            self.shooting = False
 
     def update_hitbox(self):
         #met à jour la hitbox
-        self.hitbox_enemy = pygame.Rect (
-        self.rect.x + (self.width - self.hitbox_w) // 2,
-        self.rect.y + (self.height - self.hitbox_h) // 2,
-        self.hitbox_w,
-        self.hitbox_h
-    )
+        if self.direction == -1:
+            self.hitbox_enemy = pygame.Rect (self.rect.x + (self.width - self.hitbox_w) // 2 + 11, self.rect.y + (self.height - self.hitbox_h) // 2 + 38, self.hitbox_w, self.hitbox_h)
+        elif self.platform.platform_id == 11 and self.direction == -1:
+            self.hitbox_enemy = pygame.Rect(self.rect.x + (self.width - self.hitbox_w) // 2 + 25, self.rect.y + (self.height - self.hitbox_h) // 2 + 38, self.hitbox_w, self.hitbox_h)
+        elif self.platform.platform_id == 11 and self.direction == 1:
+            self.hitbox_enemy = pygame.Rect(self.rect.x + (self.width - self.hitbox_w) // 2 - 14, self.rect.y + (self.height - self.hitbox_h) // 2 + 38, self.hitbox_w, self.hitbox_h)
+        else:
+            self.hitbox_enemy = pygame.Rect(self.rect.x + (self.width - self.hitbox_w) // 2 - 8, self.rect.y + (self.height - self.hitbox_h) // 2 + 38, self.hitbox_w, self.hitbox_h)
 
-    def draw(self, window):
+    def draw(self, surface):
+        if self.platform.platform_id in [9, 10]:
+            image = self.image2
+        elif self.platform.platform_id == 11:
+            image = self.image3
+        else:
+            image = self.image1
 
-        pygame.draw.rect(window, 'green', self.rect)
-        pygame.draw.rect(window, 'black', self.hitbox_enemy, 2)
+        if self.direction == 1:
+            image = pygame.transform.flip(image, True, False)
 
-    def move (self, dx, dy):
-        #on garde l'ancienne position
-        old_ex, old_ey = self.x, self.y
+        surface.blit(image, self.rect.topleft)
+        pygame.draw.rect(surface, (255, 0, 0), self.hitbox_enemy, 2)
 
-        self.rect.x += dx * velocity
-        self.rect.y += dy * velocity
 
-        self.limite_x = (60, 1340)
-        self.limite_y = (60, 740)
+    def update(self, player):
+        self.detect_player(player)
 
-        #MàJ de l'ennemie
-        #self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
-
-        if self.rect.left < self.limite_x[0] or self.rect.right > self.limite_x[1]:
-            self.rect.x = old_ex
-            #self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
-
-        if self.rect.top < self.limite_y[0] or self.rect.bottom > self.limite_y[1]:
-            self.rect.y = old_ey
-            #self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        # movement when not shooting
+        if not self.shooting:
+            self.rect.x += self.speed * self.direction
+        # patrolling pattern
+        #edge_offset = 2
+        if (self.hitbox_enemy.left <= self.platform.rect.left and self.direction == -1) or (self.hitbox_enemy.right >= self.platform.rect.right and self.direction == 1):
+            self.direction *= -1
 
         self.update_hitbox()
-
-    def move_towards_player(self, player, ):
-        #calculer la direction vers le joueur
-        dx = player.rect.x - self.rect.x
-        dy = player.rect.y - self.rect.y
-
-        #calculer la distance entre l'ennemi et le joueur
-        dist = math.hypot(dx, dy)
-
-        # Enemy AI: Move towards the player
-        if dist != 0:
-            dx /= dist
-            dy /= dist
-
-            #déplacer l'ennemi
-            self.rect.x += dx * (self.velocity -2)
-            self.rect.y += dy * (self.velocity -2)
-
-            self.update_hitbox()
 
     def reset(self):
         self.rect.topleft = (self.x, self.y)
@@ -179,12 +337,32 @@ class Collide_damage():
         self.max_damage = max_damage
 
     def show_death_screen(self,screen):
+        #size and position of the death window
+        death_screen_width = 400
+        death_screen_height = 200
+        death_screen_x = (screen.get_width() - death_screen_width) //2 #center horizental
+        death_screen_y = (screen.get_height() - death_screen_height) //2 #center verticaly
+
+        #create a surface semi transparant for the backscreen
+        overlay = pygame.Surface((screen.get_width(), screen.get_height()), pygame.SRCALPHA)
+        overlay.fill((0,0,0, 128)) #light black 128/255
+
+        #font and text
         font = pygame.font.Font(None, 74)
         text = font.render("You're dead dead!", True, (255, 0, 0))
-        text_rect = text.get_rect(center=(400, 200))
+        text_rect = text.get_rect(center=(death_screen_width //2,50))
 
-        retry_button = pygame.Rect(250,300,200,50)
-        exit_button = pygame.Rect(250, 400, 200, 50)
+        #buttons
+        retry_button = pygame.Rect(50,100,120,50)
+        exit_button = pygame.Rect(230, 100, 120, 50)
+
+        #text ont the buttons
+        font = pygame.font.Font(None, 36)
+        retry_text = font.render("Retry", True, (0, 0, 0)) #black text
+        retry_text_rect = retry_text.get_rect(center=retry_button.center)#center on the buttons
+
+        exit_text = font.render("Exit", True, (0, 0, 0)) #black text
+        exit_text_rect = exit_text.get_rect(center=exit_button.center) #center on the button
 
         while True:
             for event in pygame.event.get():
@@ -192,30 +370,50 @@ class Collide_damage():
                     pygame.quit()
                     sys.exit()
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    if retry_button.collidepoint(event.pos):
+                    #ajust the coordination of the mouse with the window of death
+                    mouse_x, mouse_y = event.pos
+                    relative_mouse_x = mouse_x - death_screen_x
+                    relative_mouse_y = mouse_y - death_screen_y
+
+                    if retry_button.collidepoint(relative_mouse_x, relative_mouse_y):
                         print("retry")
                         return "retry"
-                    if exit_button.collidepoint(event.pos):
+                    if exit_button.collidepoint(relative_mouse_x, relative_mouse_y):
                         print("exit")
                         return "exit"
 
+            #draw the back screen
+            screen.blit(overlay, (0, 0))
+
             #draw the window of death
-            screen.fill((0,0,0))
-            screen.blit(text, text_rect)
+            death_screen = pygame.Surface((death_screen_width, death_screen_height))
+            death_screen.fill((50, 50, 50))
+            death_screen.blit(text, text_rect)
 
             #draw the buttons
-            pygame.draw.rect(screen, (0,255,0), retry_button)
-            pygame.draw.rect(screen, (255,0,0), exit_button)
+            pygame.draw.rect(death_screen, (0,255,0), retry_button)
+            pygame.draw.rect(death_screen, (255,0,0), exit_button)
 
-            #text on the button
-            font = pygame.font.Font(None, 36)
-            retry_text = font.render("Retry", True, (0, 0, 0))
-            exit_text = font.render("Exit", True, (0, 0, 0))
-            screen.blit(retry_text, (retry_button.x +70, retry_button.y + 15))
-            screen.blit(exit_text, (exit_button.x + 70, exit_button.y + 15))
+            #draw the text on the butons
+            death_screen.blit(retry_text, retry_text_rect)
+            death_screen.blit(exit_text, exit_text_rect)
+
+            screen.blit(death_screen, (death_screen_x, death_screen_y))
 
             pygame.display.flip()
 
-pygame.init()
+class Projectiles:
+    def __init__(self, x, y, radius, color, direction, shooter):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.direction = direction
+        self.velocity = 10 * direction
+        self.shooter = shooter
 
-velocity = 12
+    def update(self):
+        self.x += self.velocity
+
+    def draw(self, surface):
+        pygame.draw.circle(surface, self.color, (self.x, self.y), self.radius)
--- src/environment.py
+++ src/environment.py
@@ -1,32 +1,86 @@
 import pygame
+import csv
+
+#setup pygame
+screen = pygame.display.set_mode((1400, 800),pygame.RESIZABLE)
+screen_width, screen_height = screen.get_size()
+
+#define game variable
+ROWS = 16
+COLS = 150
+TILE_SIZE = screen_height // ROWS
+TILE_TYPES = 12
 
 # defining gravity value
 gravity = 9.81
 
+#store tiles in a list
+img_list = []
+for x in range(TILE_TYPES):
+    img = pygame.image.load(f'image/tile/{x}.png')
+    img = pygame.transform.scale(img, (TILE_SIZE, TILE_SIZE))
+    img_list.append(img)
+
+for img in img_list:
+    print(f"Image size: {img.get_width()}x{img.get_height()}")
+
 class Platform:
-    def __init__(self, x, y, width, height, moving=False, speed=2, move_range=100):
-        self.rect = pygame.Rect(x, y, width, height)
-        self.moving = moving
-        self.speed = speed
-        self.move_range = move_range
-        self.start_x = x  # initial position
-        self.direction = 1  # 1 = right, -1 = left
+    def __init__(self, grid_x, grid_y, width_in_blocks, height_in_blocks, platform_id=None, visible=True):
+        self.tile_size = TILE_SIZE
+        self.rect = pygame.Rect(grid_x * self.tile_size, grid_y * self.tile_size, width_in_blocks * self.tile_size, height_in_blocks * self.tile_size)
+        self.platform_id = platform_id
+        self.visible = visible
 
-    def update(self):
-        # updates the mobile platform's position
-        if self.moving:
-            self.rect.x += self.speed * self.direction
-            if abs(self.rect.x - self.start_x) >= self.move_range:
-                self.direction *= -1  # invert the direction
+    def update_position(self, screen_scroll):
+        self.rect.x += screen_scroll
 
-    def draw(self, surface, color=(255, 255, 255)):
-        # draws the platform
-        pygame.draw.rect(surface, color, self.rect)
+    def draw(self, surface, color=(255, 0, 0)):
+        if self.visible:
+            pygame.draw.rect(surface, color, self.rect)
 
+class Worlds():
+    def __init__(self):
+        self.obstacles_list = [] #store tiles in a list
 
-# Création des plateformes: à priori à mettre dans la boucle, après l'initiation de Pygame
-#platforms = [
-    #Platform(200, 175, 400, 50),
-    #Platform(700, 350, 350, 50),
-    #Platform(350, 550, 250, 50, moving=True, speed=3, move_range=250)
-#]
+    # create empty tyle list
+    def load_level(self, level):
+        world_data = []
+        for row in range(ROWS):
+            r = [-1] * COLS
+            world_data.append(r)
+        # load in level data and create world
+        with open(f'level{level}_data.csv', newline='') as csvfile:
+            reader = csv.reader(csvfile, delimiter=';')
+            for row in reader:
+                world_data.append([int(tile) for tile in row])
+        return world_data
+
+    def proccess_data(self, data):
+        print("process data")
+        #iterate through each value in level data file
+        for y, row in enumerate(data):
+            for x, tile in enumerate(row):
+                if tile >= 0:
+                    img = img_list[tile]
+                    img_rect = img.get_rect()
+                    img_rect.x = (x * TILE_SIZE)
+                    img_rect.y = (y * TILE_SIZE)
+                    print(f"Tile at ({img_rect.x}, {img_rect.y})")  # Affiche les coordonnées de la tuile
+                    tile_data = (img, img_rect)
+
+                    if tile == 0:
+                        self.obstacles_list.append(tile_data)
+                    elif tile >=0 and tile <= 8:
+                        self.obstacles_list.append(tile_data)
+                    elif tile >= 8 and tile <= 8:
+                        pass #decortion
+                    elif tile == 10: #create a player
+                        pass
+
+        # Affichage pour vérifier que les tuiles ont été ajoutées
+        print(f"Obstacles list contains {len(self.obstacles_list)} tiles")
+
+    def draw(self, screen_scroll):
+        for tile in self.obstacles_list:
+            tile[1][0] += screen_scroll
+            screen.blit(tile[0], tile[1])
